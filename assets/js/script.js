4723a13b436480e4e56fe2aaafeb2128c7f154a7
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-03-03 16:45:13 +0000

    kick off modal and new game modal added to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-    $('.kick-off').fadeIn(1000).removeClass('hidden');
-    $("#logo-image").slideDown(1000).removeClass('hidden');
+    $('.kick-off').fadeIn(1000).removeClass('hidden'); //fades in kick off button on page load
+    $("#logo-image").slideDown(1000).removeClass('hidden'); //slides down logo on page load
+    
+    $("#rules-button").click(function() {
+        $("#rules-info").slideToggle("medium").removeClass("hidden"); //slides down game rules when rules button is clicked
+    })
 })
