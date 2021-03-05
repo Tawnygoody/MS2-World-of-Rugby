7b22691002eaa76f86a994894652243a7465145f
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-03-05 10:03:48 +0000

    cardsMatched & unflipCards functions added to scripts.js

---
+++ README.md
@@ -0,0 +1,14 @@
+## Media
+Images 
+- Home Page Background - [View](https://www.pixelstalk.net/free-new-zealand-all-black-rugby-hd-backgrounds/)
+- Logo Rugby Ball - [View](https://www.modestvintageplayer.com/collections/mvp-leather-balls)
+- Game page Background - [View](https://commons.wikimedia.org/wiki/File:Rugby_union_pitch.svg)
+- Card Front Image - [View](https://nbcsportsgrouppressbox.com/2018/02/02/nbc-sports-group-begins-inaugural-season-of-natwest-6-nations-championship-rugby-coverage-this-weekend/)
+- England Rugby Logo - [View](https://leadersinsport.com/company/england-rugby/attachment/logo-england-rugby/)
+- Scottish Rugby Logo - [View](https://www.talkingrugbyunion.co.uk/scottish-rugby-are-you-backing-blue-/10165.htm)
+- Japan Rugby Logo - [View](https://www.pngitem.com/middle/wTJbbJ_cherry-blossom-japan-rugby-hd-png-download/)
+- Wales Rugby Logo - [View](https://www.pinterest.at/pin/736620082776008846/)
+- France Rugby Logo - [View](https://www.news24.com/sport/Rugby/french-rugby-clubs-raise-funds-to-help-coronavirus-victims-carers-20200328)
+- New Zealand Rugby Logo - [View](https://www.pinterest.it/pin/94646029638529210/)
+- Australia Rugby Logo - [View](https://twitter.com/wallabies)
+- Argentina Rugby Logo - [View](http://www.unioncordobesaderugby.com.ar/es/2018/09/concentracion-nacional-m20-en-cordoba/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,47 @@
+const cards = document.querySelectorAll(".gameCard"); //selects all elements with a class of gameCard and stores them in variable - cards
+let cardFlipped = false;
+let firstCard, SecondCard;
+
 $(document).ready(function() {
     $('.kick-off').fadeIn(1000).removeClass('hidden'); //fades in kick off button on page load
     $("#logo-image").slideDown(1000).removeClass('hidden'); //slides down logo on page load
-    
     $("#rules-button").click(function() {
         $("#rules-info").slideToggle("medium").removeClass("hidden"); //slides down game rules when rules button is clicked
     })
+
+    
+
+    function flipCard() {
+        this.classList.add("flip"); //adds class of flip when a gameCard is clicked
+
+        if(!cardFlipped) {
+            cardFlipped = true;
+            firstCard = this;
+            return;
+        } else {
+            secondCard = this;
+            cardFlipped = false
+        }
+
+        cardsMatched()
+    }
+
+    function cardsMatched() { //cards are matched based on "data-type" and stored in variable isMatch
+        let isMatch = firstCard.dataset.logo === secondCard.dataset.logo; isMatch ? lockCards() : unflipCards();
+    }
+
+    function lockCards() { //cards are locked in place by removing the event listener for a click event.
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEvenetListener("click", flipCard);
+    }
+
+    function unflipCards() { //cards are returned 
+        setTimeout(() => {
+            firstCard.classList.remove("flip");
+            secondCard.classList.remove("flip");
+        }, 1000);
+    }
+
+
+    cards.forEach(card => card.addEventListener("click", flipCard)); //flipCard function executed when a gameCard is clicked
 })
