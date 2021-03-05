9fd7352c494ec42c4cb2abf5832eb78486eb5110
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-03-05 11:57:18 +0000

    audio controller added to scripts.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll(".gameCard"); //selects all elements with a class of gameCard and stores them in variable - cards
 let cardFlipped = false;
+let lockGame = false;
 let firstCard, SecondCard;
 
 $(document).ready(function() {
@@ -12,16 +13,17 @@ $(document).ready(function() {
     
 
     function flipCard() {
+        if(this === firstCard) return;
         this.classList.add("flip"); //adds class of flip when a gameCard is clicked
 
         if(!cardFlipped) {
             cardFlipped = true;
             firstCard = this;
             return;
-        } else {
-            secondCard = this;
-            cardFlipped = false
         }
+
+        secondCard = this;
+        
 
         cardsMatched()
     }
@@ -35,12 +37,13 @@ $(document).ready(function() {
         secondCard.removeEvenetListener("click", flipCard);
     }
 
-    function unflipCards() { //cards are returned 
-        setTimeout(() => {
+    function unflipCards() { //cards are returned to card-back image when cards do not match.
+        setTimeout(() => { //timeout allows the user to see both cards for 1 second before turning back
             firstCard.classList.remove("flip");
             secondCard.classList.remove("flip");
         }, 1000);
     }
+
 
 
     cards.forEach(card => card.addEventListener("click", flipCard)); //flipCard function executed when a gameCard is clicked
