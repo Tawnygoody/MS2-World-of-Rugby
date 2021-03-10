5a04c773bb1f6897261b6a7afa4e2f1ffff590db
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-03-10 10:34:46 +0000

    basic javascript functionality added from tutorial

--- assets/js/game-script.js
+++ assets/js/game-script.js
@@ -1,62 +1,64 @@
-if(document.readystate === "loading") {
-    document.addEventListener("DOMContentLoaded", ready());
-} else {
-    ready();
-}
+const cards = Array.from(document.getElementsByClassName("game-card")); //creates an array of all the game-cards. 
 
-const cards = document.querySelectorAll(".game-card");
-cards.forEach(card => card.addEventListener("click", flipCard));
-
-function ready() {
-
-}
-
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.add("flip");
-}
 
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
 
-function startGame() {
+        return;
+    }
 
-}
+        hasFlippedCard = false;
+        secondCard = this;
 
-function disabled() {
-
-}
-
-function shuffleCards() {
-
+        checkForMatch();
 }
 
 function checkForMatch() {
-
+    if(firstCard.dataset.logo === secondCard.dataset.logo) {
+        matchedCards();
+    } else {
+        unmatchedCards();
+    }
 }
 
-function cardMatch() {
+function matchedCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
 
+    resetBoard();
 }
 
-function notCardMatch() {
+function unmatchedCards() {
+    lockBoard = true;
 
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+
+        resetBoard();
+    }, 1000);
 }
 
-function cardType() {
-
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
-function canFlipCard() {
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+})();
 
-}
-
-function victory() {
-
-}
-
-function gameOver() {
-
-}
-
-function startCountDown() {
-
-}
+cards.forEach(card => card.addEventListener("click", flipCard)); //adds an event listener to each game-card and calls flipcard function when clicked.
