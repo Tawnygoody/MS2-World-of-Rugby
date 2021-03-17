20c0dee889c12eb60318fb3590d0faa2f8947fa6
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-03-17 13:57:38 +0000

    email js set up for contact us form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
+function sendMail(contactForm) {
+        emailjs.send("service_drsgi1h", "contact_form", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "customer_request": contactForm.customersummary.value
+        })
+        .then(
+            function(response) {
+                console.log("Success", response);
+            },
+            function(error) {
+                console.log("Failed", error);
+            });
+            return false
+    }
+
+
 $(document).ready(function() {
     $('.kick-off').fadeIn(1000).removeClass('hidden'); //fades in kick off button on page load
     $("#logo-image").slideDown(1000).removeClass('hidden'); //slides down logo on page load
     $("#rules-button").click(function() {
         $("#rules-info").slideToggle("medium").removeClass("hidden"); //slides down game rules when rules button is clicked
     });
+    $("#contact-button").click(function() {
+        $("form").slideToggle("medium").removeClass("hidden");
+    })
 });
